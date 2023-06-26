746ab0bba6fde4a4733b62ec437dd70ea72c1df5
JEPTUI <alicejeptui@gmail.com>
2023-06-25 21:47:03 -0700

    Create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,10 @@
-file { '/tmp/school':
-ensure  => file,
-content => 'I love Puppet',
-group   => 'www-data',
-mode    => '0744',
-owner   => 'www-data',
-path    => '/tmp/school',
+# create a file in /tmp
+
+file { '/tmp/holberton':
+  ensure  => file,
+  path    => '/tmp/holberton',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
 }
