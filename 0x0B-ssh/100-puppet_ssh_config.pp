467b75f3c70ddc6f18bab66368976aeba04b90c3
JEPTUI <alicejeptui@gmail.com>
2023-06-27 06:36:01 -0700

    Client configuration file (w/ Puppet) #advanced

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,7 @@
-class ssh_client_config {
-  file { '/etc/ssh/100-puppet_ssh_config':
-    ensure  => present,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    content => template('100-puppet_ssh_config/ssh_config.erb'),
-  }
+file { '/etc/ssh/ssh_config':
+  content => "
+    Host *
+      PasswordAuthentication no
+      IdentityFile ~/.ssh/school
+  "
 }
