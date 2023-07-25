eb8b47992581753f5d36d62b3d1c58d5ca8e834e
JEPTUI <alicejeptui@gmail.com>
2023-07-24 19:29:50 -0700

    alphabet fix

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -3,9 +3,9 @@
 Using what you did in the task #0, extend your Python script to
 export data in the JSON format.import argv
 """
-import sys
 import json
 import requests
+import sys
 
 
 def export_to_json():
