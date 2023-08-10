3ef3968f1d6a5f2729aeed29079f2bb3afa7c320
JEPTUI <alicejeptui@gmail.com>
2023-08-10 02:32:54 -0700

    Recursive function

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -10,15 +10,15 @@ def count_words(subreddit, word_list, instances=None, after="", count=0):
     if instances is None:
         instances = {}
 
+    headers = {'User-Agent': 'CustomUserAgent'}
+
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
     params = {
         "after": after,
         "count": count,
         "limit": 100
     }
+
     response = requests.get(url, headers=headers, params=params, allow_redirects=False)
 
     try:
